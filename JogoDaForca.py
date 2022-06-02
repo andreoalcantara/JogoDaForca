bf40294cf2d9ba5459d67b688dcf49b566a38fd9
andreoalcantara <54860118+andreoalcantara@users.noreply.github.com>
2022-06-02 17:52:17 -0300

    Add files via upload

--- JogoDaForca.py
+++ JogoDaForca.py
@@ -1,62 +1,146 @@
-''' Ex1
-from math import sqrt
+#=========================================JOGO DA FORCA=========================================
+import random
+
+# Função para ler uma palavra de forma aleatória do banco de palavras
+def rand_word():
+    f = open('palavras.txt','r')
+    f = f.read()
+    f = f.split(',')
+    posi = random.randint(0,len(f)-1)
+    print(f[posi])
+    return f[posi]
+
+tabuleiro = ['''
+
+>>>>>>>>>>Hangman<<<<<<<<<<
+
++---+
+|   |
+    |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+    |
+    |
+    |
+=========''', '''
+
++---+
+|   |
+O   |
+|   |
+    |
+    |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|   |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+     |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/    |
+     |
+=========''', '''
+
+ +---+
+ |   |
+ O   |
+/|\  |
+/ \  |
+     |
+=========''']
+
+class Hangman:
+
+    # Construtor
+    def __init__(self, word):
+        self.word = word
+        self.missed_letters = list()
+        self.right_letters = list()
+
+    # Método para tentativa de letra
+    def guess(self, letter):
+        if letter in self.word and letter not in self.right_letters:
+            self.right_letters.append(letter)
+        elif letter not in self.word and letter not in self.missed_letters:
+            self.missed_letters.append(letter)
+        else: return False
+        return True
+
+    # Método para verificar se o jogo terminou
+    def hangman_over(self):
+        return self.hangman_won() or (len(self.missed_letters)==6)
+
+    # Método para verificar se o jogador venceu
+    def hangman_won(self):
+        if '_' not in self.hide_word():return True
+        else:return False
+
+    # Método para não mostrar a letra no tabuleiro
+    def hide_word(self):
+        aux =''
+        for letter in self.word:
+            if letter not in self.right_letters:
+                aux += '_'
+            else: aux += letter
+            return aux
+
+    # Método para checar o status do game e imprimir o tabuleiro na tela
+    def print_game_status(self):
+        print(tabuleiro[len(self.missed_letters)])
+        print('Palavra: ' + self.hide_word())
+        print('Letras erradas: ')
+        for letter in self.missed_letters:
+            print(letter)
 
 
-class Rocket():
-
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-
-    def move_rocket(self, x_increment=0, y_increment=1):
-        self.x += x_increment
-        self.y += y_increment
-
-    def print_rocket(self):
-        print(self.x, self.y)
-
-foguete = Rocket(15,25)
-foguete.print_rocket()
-
-foguete.move_rocket(-5,17)
-foguete.print_rocket()
-'''
-
-'''Ex2
-class Pessoa():
-    def __init__(self, nome, cidade, telefone, email, idade):
-        self.nome = nome
-        self.cidade = cidade
-        self.telefone = telefone
-        self.email = email
-        self.idade = idade
-
-    def __str__(self):
-        return self.nome
-
-    def __len__(self):
-        return self.idade
-
-alan = Pessoa('Alan','Udia','(34)99798556','mail.alan@checkup.com',30)
-print(alan)
-'''
+# Função para ler uma palavra de forma aleatória do banco de palavras
 
 
-# Ex 3
+# Função Main - Execução do Programa
+def main():
+    # Objeto
+    game = Hangman(rand_word())
 
-class Smartphone():
+    # Enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
+    while not game.hangman_over():
+        game.print_game_status()
+        print('\n')
+        letra_usuario = input('Digite uma letra: ')
+        game.guess(letra_usuario)
 
-    def __init__(self, tamanho, interface):
-        self.tamanho = tamanho
-        self.interface = interface
+    # Verifica o status do jogo
+    game.print_game_status()
+
+    # De acordo com o status, imprime mensagem na tela para o usuário
+    if game.hangman_won():
+        print('\nVocê venceu!!')
+    else:
+        print('\nVocê perdeu!!.')
+        print('A palavra era ' + game.word)
 
 
-class MP3(Smartphone):
 
-    def __init__(self, tamanho, interface, capacidade):
-        super().__init__(tamanho, interface)
-        self.capacidade = capacidade
-        print('MP3 criado!')
-
-
-mp3 = MP3('12', 'Android', '16')
+# Executa o programa
+if __name__ == "__main__":
+    main()
